26ffe0dd20835b750cc0affa8cde9fb3ffc9e973
HubertK11 <115348420+HubertK11@users.noreply.github.com>
2024-02-15 22:22:20 +0100

    Calculator

---
+++ Calculator/src/main/java/ujd/calculator/Model/Calculator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ujd.calculator.Model;
+
+import ujd.calculator.Controller.CalculatorController;
+
+
+public class Calculator extends CalculatorController {
+    
+    CalculatorController calculator = new CalculatorController();
+    
+}
